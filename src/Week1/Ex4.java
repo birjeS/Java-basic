adad14703c5fa86a7b819f8074f1ee0092fc0f9e
Birje <birje.solatte@voco.ee>
2023-09-12 08:58:23 +0300

    Week 1

---
+++ src/Week1/Ex4.java
@@ -0,0 +1,47 @@
+package Week1;
+
+public class Ex4 {
+    public static void main(String[] args) {
+    /*   // //defining variables
+        int numberOfChickens = 3;
+        double numberOfBacon = 5.5;
+        String text = "There is none!";
+
+        // adding all together
+        //test one
+        System.out.println("Chickens: "+ numberOfChickens);
+        System.out.println("Bacon (kg):" + numberOfBacon);
+        System.out.println("A tractor:" + text);
+
+        System.out.println("In a nutshell:" + numberOfChickens + numberOfBacon + text);
+
+        //test two
+        System.out.println("Chickens:");
+        System.out.println(numberOfChickens);
+        System.out.println("Bacon (kg):");
+        System.out.println(numberOfBacon);
+        System.out.println("A tractor");
+        System.out.println(text);
+
+        System.out.println("In a nutshell:");
+        System.out.println(numberOfChickens);
+        System.out.println(numberOfBacon);
+        System.out.println(text); */
+
+        int numberOfChickens = 9000;
+        double numberOfBacon = 0.1;
+        String text = "Zetor";
+
+        System.out.println("Chickens:");
+        System.out.println(numberOfChickens);
+        System.out.println("Bacon (kg):");
+        System.out.println(numberOfBacon);
+        System.out.println("A tractor");
+        System.out.println(text);
+
+        System.out.println("In a nutshell:");
+        System.out.println(numberOfChickens);
+        System.out.println(numberOfBacon);
+        System.out.println(text);
+    }
+}
---
+++ src/Week1/Ex5.java
@@ -0,0 +1,18 @@
+package Week1;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int seconds = 60;
+        int minutes = 60;
+        int hour = 24;
+        int month = 12;
+        int days = 365;
+
+/* //how many seconds in a minute
+        System.out.println(seconds * minutes);
+        //seconds in a day
+        System.out.println(3600*24); */
+        //seconds in a year
+        System.out.println(86400*days);
+    }
+}
