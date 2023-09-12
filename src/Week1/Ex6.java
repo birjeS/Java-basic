c2ba25ff493d5035104424101bb3dc5688231108
Birje <birje.solatte@voco.ee>
2023-09-12 09:17:40 +0300

    Week 1

---
+++ src/Week1/Ex6.java
@@ -0,0 +1,22 @@
+package Week1;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        System.out.println("4 + 5 =" + (a+b));
+
+        int c = 73457;
+        int d = 12888;
+        System.out.println("73547+12888 = " + (c+d));
+
+        int e = 2;
+        int f = 8;
+        System.out.println("2 * 8 =" + (e*f));
+
+        int g = 277;
+        int h = 111;
+        System.out.println("277 * 111 =" + (g*h));
+
+    }
+}
