8bcb1f014e1f6e10f1e2ad4c5e0c22a5d65ba17d
Birje <birje.solatte@voco.ee>
2023-09-12 09:19:10 +0300

    Week 1

--- src/Week1/Ex6.java
+++ src/Week1/Ex6.java
@@ -9,14 +9,5 @@ public class Ex6 {
         int c = 73457;
         int d = 12888;
         System.out.println("73547+12888 = " + (c+d));
-
-        int e = 2;
-        int f = 8;
-        System.out.println("2 * 8 =" + (e*f));
-
-        int g = 277;
-        int h = 111;
-        System.out.println("277 * 111 =" + (g*h));
-
     }
 }
---
+++ src/Week1/Ex7.java
@@ -0,0 +1,13 @@
+package Week1;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        int e = 2;
+        int f = 8;
+        System.out.println("2 * 8 =" + (e*f));
+
+        int g = 277;
+        int h = 111;
+        System.out.println("277 * 111 =" + (g*h));
+    }
+}
